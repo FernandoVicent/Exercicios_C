1f47b0f9f67cceb9ff59543071917f3db0e65436
FernandoVicent <fernandov1092@gmail.com>
2022-09-01 16:17:19 -0300

    atualizando

--- Adivinhacao/main.c
+++ Adivinhacao/main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <locale.h>
+
 
 int main(){
-    int x;
-    int y;
-    printf("digite um número: ");
-    scanf("%d", &x);
-    printf("digite um segundo número: ");
-    scanf("%d", &y);
-    int z = x * y;
-    printf("A multiplicacao de %d por %d é igual a %d\n", x, y, z);
+    int numeroSecreto = 27;
+	int chute;
+    
+	printf("digite um numero: ");
+    scanf("%d", &chute);
+    
+    printf("Seu chute foi %d \n",chute);
+    if(chute == numeroSecreto){
+    	printf("acertou o numero");
+	}
+	else{
+		if(chute > numeroSecreto){
+			printf("o numero secreto e menor que %d ", chute);
+		}
+		else{
+		
+		printf("o numero secreto e maior que %d ", chute);
+		
+		}
+	}
+    
 }
